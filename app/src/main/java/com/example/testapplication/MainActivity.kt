eaaf3ce408f82b4d26314303a763cb4ec7784362
OPN-Macbook <>
2025-09-26 13:41:42 +0700

    Code more button 5,6,7,8 and animation

--- app/src/main/java/com/example/testapplication/MainActivity.kt
+++ app/src/main/java/com/example/testapplication/MainActivity.kt
@@ -9,12 +9,10 @@ import android.util.Property
 import android.view.LayoutInflater
 import android.view.View
 import android.view.animation.AccelerateInterpolator
-import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import com.example.testapplication.base.BaseActivity
 import com.example.testapplication.canvas.CoordinateActivity
 import com.example.testapplication.databinding.ActivityMainBinding
 import com.example.testapplication.map.MapActivity
-import com.example.testapplication.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
@@ -57,8 +55,20 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         val anim3 = getObjectAnimatorShow(binding.btn3, View.TRANSLATION_Y, distance)
         val anim4 = getObjectAnimatorShow(binding.btn4, View.TRANSLATION_X, -distance)
 
+        val anim5X = getObjectAnimatorShow(binding.btn5, View.TRANSLATION_X, distance)
+        val anim5Y = getObjectAnimatorShow(binding.btn5, View.TRANSLATION_Y, -distance)
+
+        val anim6X = getObjectAnimatorShow(binding.btn6, View.TRANSLATION_X, distance)
+        val anim6Y = getObjectAnimatorShow(binding.btn6, View.TRANSLATION_Y, distance)
+
+        val anim7X = getObjectAnimatorShow(binding.btn7, View.TRANSLATION_X, -distance)
+        val anim7Y = getObjectAnimatorShow(binding.btn7, View.TRANSLATION_Y, distance)
+
+        val anim8X = getObjectAnimatorShow(binding.btn8, View.TRANSLATION_X, -distance)
+        val anim8Y = getObjectAnimatorShow(binding.btn8, View.TRANSLATION_Y, -distance)
+
         AnimatorSet().apply {
-            playTogether(anim1, anim2, anim3, anim4)
+            playTogether(anim1, anim2, anim3, anim4, anim5X, anim5Y, anim6X, anim6Y, anim7X, anim7Y, anim8X, anim8Y)
             start()
         }
 
@@ -67,6 +77,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
             btn2.visibility = View.VISIBLE
             btn3.visibility = View.VISIBLE
             btn4.visibility = View.VISIBLE
+            btn5.visibility = View.VISIBLE
+            btn6.visibility = View.VISIBLE
+            btn7.visibility = View.VISIBLE
+            btn8.visibility = View.VISIBLE
         }
     }
 
@@ -76,8 +90,20 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         val anim3 = getObjectAnimatorHide(binding.btn3, View.TRANSLATION_Y)
         val anim4 = getObjectAnimatorHide(binding.btn4, View.TRANSLATION_X)
 
+        val anim5X = getObjectAnimatorHide(binding.btn5, View.TRANSLATION_X)
+        val anim5Y = getObjectAnimatorHide(binding.btn5, View.TRANSLATION_Y)
+
+        val anim6X = getObjectAnimatorHide(binding.btn6, View.TRANSLATION_X)
+        val anim6Y = getObjectAnimatorHide(binding.btn6, View.TRANSLATION_Y)
+
+        val anim7X = getObjectAnimatorHide(binding.btn7, View.TRANSLATION_X)
+        val anim7Y = getObjectAnimatorHide(binding.btn7, View.TRANSLATION_Y)
+
+        val anim8X = getObjectAnimatorHide(binding.btn8, View.TRANSLATION_X)
+        val anim8Y = getObjectAnimatorHide(binding.btn8, View.TRANSLATION_Y)
+
         AnimatorSet().apply {
-            playTogether(anim1, anim2, anim3, anim4)
+            playTogether(anim1, anim2, anim3, anim4, anim5X, anim5Y, anim6X, anim6Y, anim7X, anim7Y, anim8X, anim8Y)
             addListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationEnd(animation: Animator, isReverse: Boolean) {
                     binding.apply {
@@ -85,6 +111,10 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
                         btn2.visibility = View.GONE
                         btn3.visibility = View.GONE
                         btn4.visibility = View.GONE
+                        btn5.visibility = View.GONE
+                        btn6.visibility = View.GONE
+                        btn7.visibility = View.GONE
+                        btn8.visibility = View.GONE
                     }
                 }
             })
